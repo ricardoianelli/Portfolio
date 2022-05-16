ee8e9fa5196efdef0d98b78c7ddfa16427f97342
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-05-16 15:20:37 -0500

    Make job rolling work

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ var jobWidgets = [];
 let jobImgSize = {width: 18, height: 38}
 let containerSize = {width: jobsContainer.offsetWidth, height: jobsContainer.offsetHeight};
 
-jobWidgets[0] = new JobWidget(0, availableJobs[0], document.getElementById("job0"), containerSize.width*0.5 - jobImgSize.width/2, containerSize.height*0.5 - jobImgSize.height/2);
-jobWidgets[1] = new JobWidget(1, availableJobs[1], document.getElementById("job1"), containerSize.width*0.4 - jobImgSize.width/2, containerSize.height*0.4 - jobImgSize.height/2);
-jobWidgets[2] = new JobWidget(2, availableJobs[2], document.getElementById("job2"), containerSize.width*0.5 - jobImgSize.width/2, containerSize.height*0.3 - jobImgSize.height/2);
-jobWidgets[3] = new JobWidget(3, availableJobs[3], document.getElementById("job3"), containerSize.width*0.6 - jobImgSize.width/2, containerSize.height*0.4 - jobImgSize.height/2);
+jobWidgets[0] = new JobWidget(0, availableJobs[0], document.getElementById("job0"), jobImgSize.width/2, jobImgSize.height/2);
+jobWidgets[1] = new JobWidget(1, availableJobs[1], document.getElementById("job1"), jobImgSize.width/2 - 50, jobImgSize.height/2 - 50);
+jobWidgets[2] = new JobWidget(2, availableJobs[2], document.getElementById("job2"), jobImgSize.width/2, jobImgSize.height/2 - 100);
+jobWidgets[3] = new JobWidget(3, availableJobs[3], document.getElementById("job3"), jobImgSize.width/2+ 50, jobImgSize.height/2- 50);
 
 let prevButton = document.getElementById("prev_job");
-prevButton.style.left = "45%";
+prevButton.style.left = "47%";
 
 let nextButton = document.getElementById("next_job");
 nextButton.style.left =  "55%";
@@ -40,22 +40,19 @@ function goToPrevJob() {
 
 function goToNextJob(e) {
     let lastIndex = jobWidgets.length-1;
-    let lastJobPos = jobWidgets[0].getPos();
-    let nextJobPos = jobWidgets[1].getPos();
-
-    jobWidgets[0].setPos(nextJobPos.x, nextJobPos.y);
+    let lastJobPos = jobWidgets[lastIndex].getPos();
     
-    // for (let i = lastIndex; i >= 0; i--) {
+    for (let i = lastIndex; i >= 0; i--) {
 
-    //     let job = jobWidgets[i];
-    //     let nextJob;
-    //     if (i > 0) {
-    //         nextJob = jobWidgets[i-1].getPos()
-    //     }
-    //     else {
-    //         nextJob = lastJobPos;
-    //     }
+        let job = jobWidgets[i];
+        let nextJob;
+        if (i > 0) {
+            nextJob = jobWidgets[i-1].getPos()
+        }
+        else {
+            nextJob = lastJobPos;
+        }
 
-    //     job.setPos(nextJob.x, nextJob.y);
-    // }
+        job.setPos(nextJob.x, nextJob.y);
+    }
 };
