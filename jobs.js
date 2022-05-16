d416e5c2d17d7aff15ef194fbdc59516749af577
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-05-16 18:15:37 -0500

    Start over again with new template

--- jobs.js
+++
@@ -1,8 +0,0 @@
-import Job from "./job.js";
-
-export const availableJobs = [
-    new Job(0, "MSc Student", "MIU", "2021-2023", "USA", "Specialized in Back-end development, developed several simple projects mainly using Java, Python and AWS."),
-    new Job(1, "Software Developer", "Stone Co.", "2019-2021", "Brazil", "Delivered high-quality back-end services mainly using C#, RESTful API's , Event Sourcing and CQRS."),
-    new Job(2, "Physics Student", "IFF", "2014-2018", "Brazil", "Greatly improved presentation skills, worked pairing microcontrolers and sensors to collect meteorological data. Also taugh lessons to low income students to prepare them for biggest brazilian physics olímpics of public schools, the OBFEP. "),
-    new Job(3, "Game Modder", "Hobbyst", "2011-2019", "Brazil", "Used Lua and some C++ to edit and include features on Tibia's MMORPG alternative servers.")
-];
