b8506c0eab0807a9eb3a458786dcb774db8bd6a0
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-05-17 23:11:53 -0500

    Add stacker game v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,12 +180,6 @@ if (selectedTheme) {
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
-else {
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    localStorage.setItem('selected-theme', 'dark')
-    localStorage.setItem('selected-icon', 'uil-sun')
-}
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
@@ -269,6 +263,127 @@ messageForm.addEventListener('submit', (event) => {
 
     }).catch((error) => {
         alert("There was an error sending your message, please try again. Sorry for the inconvenience.")
-    });
-    
+    });    
 });
+
+
+// Stacker minigame, source: https://slicker.me/javascript/tower.htm
+let canvas = document.getElementById("minigameCanvas");
+let minigameScore = document.getElementById("minigameScore");
+let gameOverText = document.getElementById("gameOverText");
+gameOverText.style.display = "none";
+
+let context = canvas.getContext("2d");
+context.font = 'bold 30px sans-serif';
+let scrollCounter, cameraY, current, mode, xSpeed;
+let ySpeed = (1/100) * canvas.height;
+let height = canvas.height/15;
+let boxes = [];
+
+boxes[0] = {
+  x: canvas.width/2.6,
+  y: canvas.height/2,
+  width: canvas.width/4
+};
+
+let debris = {
+  x: 0,
+  width: 0
+};
+ 
+function newBox() {
+  boxes[current] = {
+    x: 0,
+    y: (current + 12) * height,
+    width: boxes[current - 1].width
+  };
+}
+ 
+function gameOver() {
+  mode = 'gameOver';
+  gameOverText.style.display = "block";
+}
+ 
+function animate() {
+  if (mode != 'gameOver') {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let n = 0; n < boxes.length; n++) {
+      let box = boxes[n];
+      context.fillStyle = 'hsl(250,50%,' + (20 + n * 4) + '%)';
+      context.fillRect(box.x, canvas.height - box.y + cameraY, box.width, height);
+    }
+
+    context.fillStyle = 'orange';
+    context.fillRect(debris.x, canvas.height - debris.y + cameraY, debris.width, height);
+    if (mode == 'bounce') {
+      boxes[current].x = boxes[current].x + xSpeed;
+      if (xSpeed > 0 && boxes[current].x + boxes[current].width > canvas.width)
+        xSpeed = -xSpeed;
+      if (xSpeed < 0 && boxes[current].x < 0)
+        xSpeed = -xSpeed;
+    }
+    if (mode == 'fall') {
+      boxes[current].y = boxes[current].y - ySpeed;
+      if (boxes[current].y == boxes[current - 1].y + height) {
+        mode = 'bounce';
+        let difference = boxes[current].x - boxes[current - 1].x;
+        if (Math.abs(difference) >= boxes[current].width) {
+          gameOver();
+        }
+        debris = {
+          y: boxes[current].y,
+          width: difference
+        };
+        if (boxes[current].x > boxes[current - 1].x) {
+          boxes[current].width = boxes[current].width - difference;
+          debris.x = boxes[current].x + boxes[current].width;
+        } else {
+          debris.x = boxes[current].x - difference;
+          boxes[current].width = boxes[current].width + difference;
+          boxes[current].x = boxes[current - 1].x;
+        }
+        if (xSpeed > 0)
+          xSpeed = xSpeed + (1/1000 * canvas.width);
+        else
+          xSpeed--;
+        current++;
+        minigameScore.innerHTML = `Score: ${(current-1)}`;
+        scrollCounter = height;
+        newBox();
+      }
+    }
+    debris.y = debris.y - ySpeed;
+    if (scrollCounter) {
+      cameraY++;
+      scrollCounter--;
+    }
+  }
+
+  window.requestAnimationFrame(animate);
+}
+ 
+function restart() {
+  boxes.splice(1, boxes.length - 1);
+  mode = 'bounce';
+  cameraY = 0;
+  scrollCounter = 0;
+  xSpeed = 1;
+  current = 1;
+  newBox();
+  debris.y = 0;
+  gameOverText.style.display = "none";
+}
+ 
+canvas.onpointerdown = function() {
+  if (mode == 'gameOver')
+    restart();
+  else {
+    if (mode == 'bounce')
+      mode = 'fall';
+  }
+};
+ 
+restart();
+animate();
+
